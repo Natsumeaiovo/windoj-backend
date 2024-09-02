d65e6aa0ef36da2b1241c7c72a91c8be10ff3da3
serein <natsumeaiovo@outlook.com>
2024-09-02 17:20:16 +0800

    第三期和第四期完结。这两期后端基本无变化。

--- src/main/java/com/serein/windoj/controller/QuestionController.java
+++ src/main/java/com/serein/windoj/controller/QuestionController.java
@@ -143,7 +143,7 @@ public class QuestionController {
     }
 
     /**
-     * 根据 id 获取
+     * 根据 id 获取 QuestionVO
      *
      * @param id
      * @return
@@ -158,6 +158,29 @@ public class QuestionController {
             throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
         }
         return ResultUtils.success(questionService.getQuestionVO(question, request));
+    }
+
+    /**
+     * 根据 id 获取 Question
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/get")
+    public BaseResponse<Question> getQuestionById(long id, HttpServletRequest request) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = questionService.getById(id);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        // 如果你要获取的题目不是本人创建的，并且你还不是管理员，那么拒绝获取
+        if (!question.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResultUtils.success(question);
     }
 
     /**
